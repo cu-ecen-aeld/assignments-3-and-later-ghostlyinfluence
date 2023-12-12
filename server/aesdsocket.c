af490f492e2967a38e33744c9014079d04933a70
Colin Burgin <cburgin707@gmail.com>
2023-12-12 01:22:38 -0500

    Code working and fully tested. No mem leaks on valgrind

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -13,16 +13,21 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-// struct addrinfo {
-//     int     ai_flags;
-//     int     ai_family;
-//     int     ai_socktype;
-//     int     ai_protocol;
-//     socklen_t   ai_addrlen;
-//     struct sockaddr *ai_addr;
-//     char    *ai_canonname;
-//     struct addrinfo *ai_next;
-// };
+#define PORT "9000"
+#define BUF_LEN 1024
+
+//Globals
+int run_flag = 0;
+int sockfd = -1;
+
+static void signal_handler(int signo) {
+    if (signo == SIGINT || signo == SIGTERM) {
+        syslog(LOG_INFO, "Caught signal, exiting");
+        // shutdown the socket
+        shutdown(sockfd, SHUT_RD);
+        run_flag = 1;
+    }
+}
 
 int main(int argc, char *argv[]) {
     
@@ -30,15 +35,12 @@ int main(int argc, char *argv[]) {
     openlog(NULL, 0, LOG_USER);
     syslog(LOG_INFO, "syslog opened for aesdsocket");
 
-    // TODO: Add arg parsing for -d "Daemon command"
-
     // TODO: Handle SIGINT and SIGTERM being received
 
     // Follow what the video said to do. ALSO, the getaddrinfo man page has a great example
     struct addrinfo hints;
     struct addrinfo *servinfo, *rp;
     int status;
-    int sockfd;
     
     //Setup the socket 'hints'
     memset(&hints, 0, sizeof(hints));
@@ -52,9 +54,9 @@ int main(int argc, char *argv[]) {
     hints.ai_next = NULL;
 
     // getaddrinfo step
-    if ((status = getaddrinfo(NULL, 9000, &hints, &servinfo)) != 0) {
+    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         syslog(LOG_ERR, "getaddrinfo: %s\n", gai_strerror(status));
-        return -1;
+        exit(-1);
     }
 
     // open socket step
@@ -66,46 +68,126 @@ int main(int argc, char *argv[]) {
 
         if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) {
             break;
+        } else {
+            close(sockfd);
+            freeaddrinfo(servinfo);
+            exit(-1);
         }
-        close(sockfd);
     }
-
-    freeaddrinfo(servinfo);
 
     // Verify that it could bind
     if (rp == NULL) {
         syslog(LOG_ERR, "could not bind");
-        return -1;
+        close(sockfd);
+        freeaddrinfo(servinfo);
+        exit(-1);
     }
 
-    int connfd;
-    int backlog = 128; //Number from manpage
-
-    // Create directory and file
-    syslog(LOG_INFO, "Creating /var/tmp/aesdsocketdata if not already created");
-    const char cmd_str[] = "mkdir -p /var/tmp; touch /var/tmp/aesdsocketdata";
-    char tcmd[sizeof(cmd_str) + 1];
-    sprintf(tcmd, cmd_str);
-    return system(tcmd);
-
-    syslog(LOG_INFO, "Server starter: entering main loop");
-    // Main loop for listening, accepting, and handling connections
-    while(1) {
-        // listen for new client
-        if ((listen(sockfd, backlog)) == -1) {
-            syslog(LOG_ERR, "listen failed");
-            close(sockfd);
-            return -1;
+    // Setup Daemon if requested
+    int opt;
+    while ((opt = getopt(argc, argv, "d")) != -1) {
+        switch (opt) {
+        case 'd': 
+            if (daemon(0, 0) < 0) {
+                syslog(LOG_ERR, "daemon failed");
+                exit(-1);
+            }
+            break;
+        default:
+            printf("Usage: %s [-d]\n", argv[0]);
+            exit(-1);
         }
+    }
 
+    // Listen
+    int backlog = 128; //Number from manpage
+    if ((listen(sockfd, backlog)) == -1) {
+        syslog(LOG_ERR, "listen failed");
+        close(sockfd);
+        exit(-1);
+    }
+
+    // assign signal handlers
+    struct sigaction signal_action;
+    signal_action.sa_handler = signal_handler;
+    signal_action.sa_flags = 0;
+    sigemptyset(&signal_action.sa_mask);
+    sigaction(SIGINT, &signal_action, NULL);
+    sigaction(SIGTERM, &signal_action, NULL);
+
+    // Open and create the file if it doesn't already exist
+    int fd = open("/var/tmp/aesdsocketdata", O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) {
+        syslog(LOG_ERR, "open failed");
+        exit(-1);
+    }
+
+    while(!run_flag) {
         // accept connection
-        struct sockaddr_storage clientaddr;
+        struct sockaddr_in clientaddr;
         socklen_t clientaddrlen = sizeof(clientaddr);
-        connfd = accept(sockfd, (struct sockaddr *)&clientaddr, clientaddrlen);
+        int connfd = accept(sockfd, (struct sockaddr *)&clientaddr, &clientaddrlen);
         if (connfd == -1) {
             syslog(LOG_ERR, "accept failed");
-            return -1;
+            exit(-1);
         }
 
-    
+        // Logs message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client.
+        syslog(LOG_INFO, "Accepted connection from %s", inet_ntoa(clientaddr.sin_addr));
+
+        // Handle reading and writing
+        char buf[BUF_LEN];
+        buf[BUF_LEN - 1] = '\0';
+        int eol = 0;
+        while(eol == 0) {
+            int read_bytes = read(connfd, buf, sizeof(buf));
+            if (read_bytes < 0) {
+                syslog(LOG_ERR, "read failed");
+                exit(-1);
+            } else if (read_bytes == 0) {
+                //EOF
+                break;
+            } else {
+                // find the new line
+                char *new_line_ptr = strchr(buf, '\n');
+                if (new_line_ptr != NULL) {
+                    read_bytes = (new_line_ptr - buf) + 1;
+                    eol = 1;
+                }
+
+                int write_bytes = write(fd, buf, read_bytes);
+                if (write_bytes < 0) {
+                    syslog(LOG_ERR, "write failed");
+                    exit(-1);
+                }
+            }
+        }
+
+        lseek(fd, 0, SEEK_SET);
+
+        while(1) {
+            int read_bytes = read(fd, buf, sizeof(buf));
+            if (read_bytes < 0) {
+                syslog(LOG_ERR, "read failed");
+                exit(-1);
+            } else if (read_bytes == 0) {
+                //EOF
+                break;
+            } else {
+                int write_bytes = write(connfd, buf, read_bytes);
+                if (write_bytes < 0) {
+                    syslog(LOG_ERR, "write failed");
+                    exit(-1);
+                }
+            }
+        }
+
+        // syslog that connection closed
+        syslog(LOG_INFO, "Closed connection from %s", inet_ntoa(clientaddr.sin_addr));
+        close(connfd);
+    }
+
+    close(fd);
+    close(sockfd);
+    unlink("/var/tmp/aesdsocketdata");
 }
