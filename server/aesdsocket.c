7bed6d35073521447487418c149e4b170a96e454
Colin Burgin <cburgin707@gmail.com>
2023-12-12 02:00:47 -0500

    Fixes

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -13,7 +13,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT "9000"
+#define PORT 9000
 #define BUF_LEN 1024
 
 //Globals
@@ -35,53 +35,79 @@ int main(int argc, char *argv[]) {
     openlog(NULL, 0, LOG_USER);
     syslog(LOG_INFO, "syslog opened for aesdsocket");
 
-    // TODO: Handle SIGINT and SIGTERM being received
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        syslog(LOG_ERR, "socket failed");
+        exit(-1);
+    }
 
-    // Follow what the video said to do. ALSO, the getaddrinfo man page has a great example
-    struct addrinfo hints;
-    struct addrinfo *servinfo, *rp;
-    int status;
+    // use SO_REUSEADDR to reuse the port
+    int s_opt = 1;
+    int ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &s_opt, sizeof(s_opt));
+    if (ret < 0)
+    {
+        syslog(LOG_ERR, "setsockopt failed");
+        exit(-1);
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (ret < 0)
+    {
+        syslog(LOG_ERR, "bind failed");
+        exit(-1);
+    }
+
+    // // Follow what the video said to do. ALSO, the getaddrinfo man page has a great example
+    // struct addrinfo hints;
+    // struct addrinfo *servinfo, *rp;
+    // int status;
     
-    //Setup the socket 'hints'
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = 0;
-    hints.ai_addrlen = 0;
-    hints.ai_addr = NULL;
-    hints.ai_canonname = NULL;
-    hints.ai_next = NULL;
+    // //Setup the socket 'hints'
+    // memset(&hints, 0, sizeof(hints));
+    // hints.ai_flags = AI_PASSIVE;
+    // hints.ai_family = AF_INET;
+    // hints.ai_socktype = SOCK_STREAM;
+    // hints.ai_protocol = 0;
+    // hints.ai_addrlen = 0;
+    // hints.ai_addr = NULL;
+    // hints.ai_canonname = NULL;
+    // hints.ai_next = NULL;
 
-    // getaddrinfo step
-    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        syslog(LOG_ERR, "getaddrinfo: %s\n", gai_strerror(status));
-        exit(-1);
-    }
+    // // getaddrinfo step
+    // if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    //     syslog(LOG_ERR, "getaddrinfo: %s\n", gai_strerror(status));
+    //     exit(-1);
+    // }
 
-    // open socket step
-    for (rp = servinfo; rp != NULL; rp = rp->ai_next) {
-        sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-        if (sockfd == -1) {
-            continue;
-        }
+    // // open socket step
+    // for (rp = servinfo; rp != NULL; rp = rp->ai_next) {
+    //     sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    //     if (sockfd == -1) {
+    //         continue;
+    //     }
 
-        if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) {
-            break;
-        } else {
-            close(sockfd);
-            freeaddrinfo(servinfo);
-            exit(-1);
-        }
-    }
+    //     if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+    //         break;
+    //     } else {
+    //         close(sockfd);
+    //         freeaddrinfo(servinfo);
+    //         exit(-1);
+    //     }
+    // }
 
-    // Verify that it could bind
-    if (rp == NULL) {
-        syslog(LOG_ERR, "could not bind");
-        close(sockfd);
-        freeaddrinfo(servinfo);
-        exit(-1);
-    }
+    // // Verify that it could bind
+    // if (rp == NULL) {
+    //     syslog(LOG_ERR, "could not bind");
+    //     close(sockfd);
+    //     freeaddrinfo(servinfo);
+    //     exit(-1);
+    // }
 
     // Setup Daemon if requested
     int opt;
@@ -190,4 +216,6 @@ int main(int argc, char *argv[]) {
     close(fd);
     close(sockfd);
     unlink("/var/tmp/aesdsocketdata");
+    closelog();
+    return 0;
 }
